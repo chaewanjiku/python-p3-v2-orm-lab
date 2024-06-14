34e5169f288a608d4e5a21e6b825fcf8ab9f4f9c
Charity <charitywanjiku8245@gmail.com>
2024-06-14 22:34:48 +0300

    Lab complete

--- lib/review.py
+++ lib/review.py
@@ -14,6 +14,36 @@ class Review:
         self.summary = summary
         self.employee_id = employee_id
 
+    @property
+    def year(self):
+        return self._year
+    @year.setter
+    def year(self,year):
+        if isinstance(year, int) and year >= 2000:
+            self._year = year
+        else:
+            raise ValueError('Input is an invalid year')  
+
+    @property
+    def summary(self):
+        return self._summary
+    @summary.setter
+    def summary(self,summary):
+        if isinstance(summary, str) and len(summary):
+            self._summary = summary
+        else:
+            raise ValueError('Input is an invalid summary')
+
+    @property
+    def employee_id(self):
+        return self._employee_id
+    @employee_id.setter
+    def employee_id(self,employee_id):
+        if type(employee_id) is int and Employee.find_by_id(employee_id):
+            self._employee_id = employee_id
+        else:
+            raise ValueError('Employee ID is not recognised')          
+        
     def __repr__(self):
         return (
             f"<Review {self.id}: {self.year}, {self.summary}, "
@@ -47,36 +77,74 @@ class Review:
         """ Insert a new row with the year, summary, and employee id values of the current Review object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
+        sql = """
+            INSERT INTO reviews (year, summary, employee_id)
+                        VALUES  (?,?,?)
+        """
+        CURSOR.execute(sql,(self.year,self.summary,self.employee_id))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
         pass
 
     @classmethod
     def create(cls, year, summary, employee_id):
         """ Initialize a new Review instance and save the object to the database. Return the new instance. """
+        review = cls(year, summary, employee_id)
+        review.save()
+        return review
         pass
    
     @classmethod
     def instance_from_db(cls, row):
         """Return an Review instance having the attribute values from the table row."""
+        review = cls.all.get(row[0])
         # Check the dictionary for  existing instance using the row's primary key
+        if review:
+            review.year = row[1]
+            review.summary = row[2]
+            review.employee_id = row[3]
+        else:
+            review = cls(row[1],row[2],row[3])
+            review.id = row[0]
+            review.all[review.id] = review
+        return review        
         pass
    
 
     @classmethod
     def find_by_id(cls, id):
         """Return a Review instance having the attribute values from the table row."""
+        sql="""
+            SELECT * FROM  reviews WHERE id= ?
+        """
+        result =  CURSOR.execute(sql, (id, )).fetchone()
+        return cls.instance_from_db(result) if result else None   
         pass
 
     def update(self):
         """Update the table row corresponding to the current Review instance."""
+        sql="""
+            UPDATE  reviews SET year=? ,summary=?, employee_id=? WHERE id=?
+        """
+        CURSOR.execute(sql, (self.year, self.summary, self.employee_id, self.id))
+        CONN.commit()
         pass
 
     def delete(self):
         """Delete the table row corresponding to the current Review instance,
         delete the dictionary entry, and reassign id attribute"""
+        sql="DELETE FROM reviews WHERE id = ?"
+        CURSOR.execute(sql,(self.id,))
+        CONN.commit()
+        del type(self).all[self.id]
+        self.id = None
         pass
 
     @classmethod
     def get_all(cls):
         """Return a list containing one Review instance per table row"""
+        sql="SELECT * FROM reviews"
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
         pass
-
